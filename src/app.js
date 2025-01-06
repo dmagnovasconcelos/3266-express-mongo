ca5b4f7da94f4087fd24d5e9f04be11c97d26df2
Danilo Vasconcelos <78868403+dmagnovasconcelos@users.noreply.github.com>
2025-01-06 19:04:56 -0300

    feat: add CRUD functionality book

--- src/app.js
+++ src/app.js
@@ -1,53 +1,17 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+conexao.on("error", (error) => {
+    console.error("Erro de conexão", error)
+});
+
+conexao.once("open", () => {
+    console.log("conexão com o banco feita com sucesso")
+});
 
 const app = express();
-app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-];
-
-function buscaLivro(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-    })
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(204).send()
-});
-
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(204).send()
-});
+routes(app);
 
 export default app;
